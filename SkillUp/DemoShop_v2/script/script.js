5aceb4603b7ea1084960bd36dadc0c6ad0b404d3
NikitaIvanchikov <n.o.ivanchikov@gmail.com>
2020-06-18 20:22:24 +0300

    added counter but not with .addEventListener method

--- SkillUp/DemoShop_v2/script/script.js
+++ SkillUp/DemoShop_v2/script/script.js
@@ -1,8 +1,15 @@
-var clicks = 0;
+let clicks = 0;
+// let addCartButton = document.getElementsByClassName('add-to-cart');
+
+
 function cartCount () {
   clicks+=1;
-  document.getElementById("clickAmoutn").textContent = clicks;
-  clickAmoutn.style.cssText=`display:block;`
+  document.getElementById("clickAmoutn").innerHTML = clicks;
+  clickAmoutn.style.cssText=`display:block;`;
+  
   // counter.innerHTML = clicks;
   console.log(clicks);
 }
+// addCartButton.addEventListener("click" , cartCount());
+
+// console.log (document.getElementsByClassName('add-to-cart'));
