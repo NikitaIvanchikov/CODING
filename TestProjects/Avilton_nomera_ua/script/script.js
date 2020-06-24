10b6b06c466b5ca0483a188aaa139094df482df2
NikitaIvanchikov <n.o.ivanchikov@gmail.com>
2020-06-24 14:55:29 +0300

    slider added

--- TestProjects/Avilton_nomera_ua/script/script.js
+++ TestProjects/Avilton_nomera_ua/script/script.js
@@ -28,3 +28,30 @@ console.log(orderToggleSeviceItem);
         orderPoupup.style.display = "flex";
       }
     }
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let slides = document.getElementsByClassName('slider__slides');
+  console.log(slides);
+
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (let i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
